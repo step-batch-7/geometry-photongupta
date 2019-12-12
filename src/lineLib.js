2e9747edcc126a69ed37531c2cf87a80918a1aea
Rashmi Gupta <rashmi224141@gmail.com>
2019-12-12 23:47:39 +0530

    Modified toString function
    modified toString function in order to get appropriate format.

--- src/lineLib.js
+++ src/lineLib.js
@@ -1,18 +1,21 @@
 class Line {
   constructor(x1, y1, x2, y2) {
-    this.point1 = { x: x1, y: y1 };
-    this.point2 = { x: x2, y: y2 };
+    this.endA = { x: x1, y: y1 };
+    this.endB = { x: x2, y: y2 };
   }
+
   toString() {
-    return JSON.stringify(this);
+    return `Line : endA(${this.endA.x}, ${this.endA.y})--------endB(${this.endB.x}, ${this.endB.y}})`;
   }
+
   isEqual(other) {
     return (
-      this.point1.x == other.point1.x &&
-      this.point1.y == other.point1.x &&
-      this.point2.x == other.point2.x &&
-      this.point2.y == other.point2.y
+      this.endA.x == other.endA.x &&
+      this.endA.y == other.endA.x &&
+      this.endB.x == other.endB.x &&
+      this.endB.y == other.endB.y
     );
   }
 }
+
 module.exports = Line;
--- test/testLine.js
+++ test/testLine.js
@@ -3,13 +3,14 @@ const assert = require("assert");
 
 describe("Line", function() {
   describe("toString", function() {
-    it("should stringify the given object", function() {
+    it("should give the string representation of the line", function() {
       let line1 = new Line(1, 2, 3, 4);
       let actual = line1.toString();
-      let expected = '{"point1":{"x":1,"y":2},"point2":{"x":3,"y":4}}';
+      let expected = `Line : endA(1, 2)--------endB(3, 4})`;
       assert.deepStrictEqual(actual, expected);
     });
   });
+
   describe("isEqual", function() {
     it(" should validate if given lines are not equal", function() {
       let line1 = new Line(1, 2, 3, 4);
@@ -18,6 +19,7 @@ describe("Line", function() {
       let expected = false;
       assert.deepStrictEqual(actual, expected);
     });
+
     it(" should validate if given lines are equal", function() {
       let line1 = new Line(1, 2, 3, 4);
       let line2 = new Line(1, 2, 3, 4);
