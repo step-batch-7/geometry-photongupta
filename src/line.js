979fadf7112421ec8aed9945dedd867fbb171894
Rashmi Gupta <rashmi224141@gmail.com>
2019-12-13 10:37:04 +0530

    Refactored the code

--- src/line.js
+++ src/line.js
@@ -1,14 +1,15 @@
-isPointsEqual = function(point1, point2) {
-  return point1.x == point2.x && point1.y == point2.y;
+const isPointsEqual = function(point1, point2) {
+  return point1.x === point2.x && point1.y === point2.y;
 };
 
 class Line {
   constructor(endA, endB) {
-    [this.endA, this.endB] = [endA, endB];
+    this.endA = { x: endA.x, y: endA.y };
+    this.endB = { x: endB.x, y: endB.y };
   }
 
   toString() {
-    return `Line : endA(${this.endA.x}, ${this.endA.y})--------endB(${this.endB.x}, ${this.endB.y}})`;
+    return `Line : endA(${this.endA.x}, ${this.endA.y}) -- endB(${this.endB.x}, ${this.endB.y}})`;
   }
 
   isEqual(otherLine) {
