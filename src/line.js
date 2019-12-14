12a2f3d072b81685bc4ac1fb67efc0dea20b7d40
Rashmi Gupta <rashmi224141@gmail.com>
2019-12-14 12:29:30 +0530

    refactored the findX and findY methods

--- src/line.js
+++ src/line.js
@@ -2,6 +2,14 @@ const arePointsEqual = function(point1, point2) {
   return point1.x === point2.x && point1.y === point2.y;
 };
 
+const isOrdinateOnTheLine = function(y, point1, point2) {
+  return !((y > point1.y && y < point2.y) || (y < point1.y && y > point2.y));
+};
+
+const isAbscissaOnTheLine = function(x, point1, point2) {
+  return !((x > point1.x && x < point2.x) || (x < point1.x && x > point2.x));
+};
+
 class Line {
   constructor(endA, endB) {
     this.endA = { x: endA.x, y: endA.y };
@@ -41,24 +49,12 @@ class Line {
   }
 
   findX = function(y) {
-    if (
-      !(
-        (y > this.endA.y && y < this.endB.y) ||
-        (y < this.endA.y && y > this.endB.y)
-      )
-    )
-      return NaN;
+    if (isOrdinateOnTheLine(y, this.endA, this.endB)) return NaN;
     return (y - this.endA.y) / this.slope + this.endA.x;
   };
 
   findY = function(x) {
-    if (
-      !(
-        (x > this.endA.x && x < this.endB.x) ||
-        (x < this.endA.x && x > this.endB.x)
-      )
-    )
-      return NaN;
+    if (isAbscissaOnTheLine(x, this.endA, this.endB)) return NaN;
     return (x - this.endA.x) * this.slope + this.endA.y;
   };
 }
