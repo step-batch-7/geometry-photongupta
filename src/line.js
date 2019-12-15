c7383a91c12a227ad83f435eb7014042bf9e8724
Rashmi Gupta <rashmi224141@gmail.com>
2019-12-15 14:34:02 +0530

    modified isParallelTo method

--- src/line.js
+++ src/line.js
@@ -11,11 +11,9 @@ const isCoordinateOnTheLine = function(coordinate, coordinateOfEnds) {
   return coordinate >= coordinateOfEnd1 && coordinate <= coordinateOfEnd2;
 };
 
-const arePointsCollinear = function(point1, point2, point3) {
-  const [x1, y1] = [point1.x, point1.y];
-  const [x2, y2] = [point2.x, point2.y];
-  const [x3, y3] = [point3.x, point3.y];
-  return x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2) == 0;
+const getYIntercept = function(point, slope) {
+  const [x, y] = [point.x, point.y];
+  return y - slope * x;
 };
 
 class Line {
@@ -53,13 +51,11 @@ class Line {
 
   isParallelTo(other) {
     if (!other instanceof Line) return false;
-    const isSlopeEqual = this.slope == other.slope;
-    const arePointsNonCollinear = !arePointsCollinear(
-      this.endA,
-      this.endB,
-      other.endA
-    );
-    return isSlopeEqual && arePointsNonCollinear;
+    const areYInterceptsNotEqual =
+      getYIntercept(this.endA, this.slope) !=
+      getYIntercept(other.endA, other.slope);
+    const areSlopesEqual = this.slope == other.slope;
+    return areSlopesEqual && areYInterceptsNotEqual;
   }
 
   findX(ordinate) {
