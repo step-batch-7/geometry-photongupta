65918443b9bbd12ff257c90760c9d6b1ee4d1b59
Rashmi Gupta <rashmi224141@gmail.com>
2019-12-16 15:22:20 +0530

    Added test cases for isEqualTo method of Circle class
    it will give false if one of the radius and centres of the circles are different.

--- test/circleTest.js
+++ test/circleTest.js
@@ -20,5 +20,29 @@ describe("Circle", function() {
       const circle2 = new Circle(point2, 5);
       assert.isTrue(circle1.isEqualTo(circle2));
     });
+
+    it("should give false if two circles have same radius and different centres", function() {
+      const point1 = new Point(0, 0);
+      const circle1 = new Circle(point1, 5);
+      const point2 = new Point(1, 1);
+      const circle2 = new Circle(point2, 5);
+      assert.isFalse(circle1.isEqualTo(circle2));
+    });
+
+    it("should give false if two circles have different radius and same centres", function() {
+      const point1 = new Point(1, 1);
+      const circle1 = new Circle(point1, 8);
+      const point2 = new Point(1, 1);
+      const circle2 = new Circle(point2, 5);
+      assert.isFalse(circle1.isEqualTo(circle2));
+    });
+
+    it("should give false if two circles have different radius and  centres", function() {
+      const point1 = new Point(0, 0);
+      const circle1 = new Circle(point1, 3);
+      const point2 = new Point(1, 1);
+      const circle2 = new Circle(point2, 5);
+      assert.isFalse(circle1.isEqualTo(circle2));
+    });
   });
 });
