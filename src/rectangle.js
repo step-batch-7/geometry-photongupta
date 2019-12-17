b9c5b0e52edd5aea6aa63383eabe2b043718444b
Rashmi Gupta <rashmi224141@gmail.com>
2019-12-17 13:59:33 +0530

    Implemented a new class--> Rectangle
    It will a rectangle of given diameter
    implemented a method which will give the string representation of the
    given rectangle.

---
+++ src/rectangle.js
@@ -0,0 +1,16 @@
+const Point = require("./point.js");
+
+class Rectangle {
+  constructor(endA, endB) {
+    this.endA = new Point(endA.x, endA.y);
+    this.endB = new Point(endB.x, endB.y);
+  }
+
+  toString() {
+    const endA = `(${this.endA.x},${this.endA.y})`;
+    const endB = `(${this.endB.x},${this.endB.y})`;
+    return `[Rectangle ${endA} to ${endB}]`;
+  }
+}
+
+module.exports = Rectangle;
---
+++ test/rectangleTest.js
@@ -0,0 +1,16 @@
+const assert = require("chai").assert;
+const Line = require("../src/line.js");
+const Point = require("../src/point.js");
+const Rectangle = require("../src/rectangle.js");
+
+describe("Rectangle", function() {
+  describe("toString", function() {
+    it("should give string representation of given rectangle", function() {
+      let endA = new Point(1, 1);
+      let endB = new Point(2, 3);
+      let rectangle = new Rectangle(endA, endB);
+      let expected = "[Rectangle (1,1) to (2,3)]";
+      assert.strictEqual(rectangle.toString(), expected);
+    });
+  });
+});
